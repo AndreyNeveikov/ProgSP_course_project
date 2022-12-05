ecf0556493f3e27dfee05c9e6882475134cbd845
AndreyNeveykov <aneveykov@datamola.com>
2022-12-05 13:05:23 +0300

    Изменен код развертывания базы данных; Добавлено приложение Б
    Добавлено приложение Б; Добавлены связи между таблицами в бд

--- Java/Course_project/src/main/java/com/example/course_project/database/DatabaseDeployment.java
+++ Java/Course_project/src/main/java/com/example/course_project/database/DatabaseDeployment.java
@@ -47,18 +47,20 @@ public class DatabaseDeployment {
                 "client_passport_number             bigint(7)   not null," +
                 "client_status                      varchar(15) not null)");
 
-        statement.executeUpdate("CREATE TABLE clients_financial_data (" +
-                "client_id                            int         primary key," +
-                "client_loan_id                       varchar(15) not null, " +
-                "client_total_debt                    double      not null," +
-                "client_monthly_income                double      not null," +
-                "client_monthly_loan_payment          double      not null," +
-                "client_loan_interest                 double      not null," +
-                "client_number_of_repaid_loans        int         not null," +
-                "client_total_number_of_loans         int         not null," +
-                "client_value_of_collateral           double      not null," +
-                "client_number_of_overdue_payments    int         not null," +
-                "client_risk_rate                     double      not null)");
+        statement.executeUpdate("CREATE TABLE fct_clients_financial_data (" +
+                "client_id                            int    primary key," +
+                "client_loan_id                       int    not null, " +
+                "employee_loaned_id                   int    not null, " +
+                "client_loaned_date                   date    not null, " +
+                "client_total_debt                    double not null," +
+                "client_monthly_income                double not null," +
+                "client_monthly_loan_payment          double not null," +
+                "client_loan_interest                 double not null," +
+                "client_number_of_repaid_loans        int    not null," +
+                "client_total_number_of_loans         int    not null," +
+                "client_value_of_collateral           double not null," +
+                "client_number_of_overdue_payments    int    not null," +
+                "client_risk_rate                     double not null)");
 
         statement.executeUpdate("CREATE TABLE loan_product (" +
                 "product_id                       int         primary key," +
@@ -82,7 +84,7 @@ public class DatabaseDeployment {
                 "employee_job_title        varchar(30) not null)");
 
         statement.executeUpdate("CREATE TABLE bank_financial_flows (" +
-                "financial_month                  date        primary key," +
+                "fin_date                         date        primary key," +
                 "bank_own_funds                   double      not null," +
                 "bank_borrowed_funds              double      not null, " +
                 "bank_reserve_funds               double      not null," +
@@ -90,6 +92,22 @@ public class DatabaseDeployment {
                 "central_bank_refinancing_rate    double      not null," +
                 "bank_monthly_expected_income     double      not null," +
                 "bank_monthly_expected_costs      double      not null)");
+
+        statement.executeUpdate("ALTER TABLE fct_clients_financial_data\n" +
+                "   ADD CONSTRAINT fk_t_fct_clients_fin_data_t_clients_pers_data FOREIGN KEY (client_id)\n" +
+                "      REFERENCES clients_personal_data (client_id);");
+
+        statement.executeUpdate("ALTER TABLE fct_clients_financial_data\n" +
+                "   ADD CONSTRAINT fk_t_fct_clients_fin_data_t_loan_product FOREIGN KEY (client_loan_id)\n" +
+                "      REFERENCES loan_product (product_id);");
+
+        statement.executeUpdate("ALTER TABLE fct_clients_financial_data\n" +
+                "   ADD CONSTRAINT fk_t_fct_clients_fin_data_t_bank_empl_auth FOREIGN KEY (employee_loaned_id)\n" +
+                "      REFERENCES bank_employee_authorization (employee_id);");
+
+        statement.executeUpdate("ALTER TABLE fct_clients_financial_data\n" +
+                "   ADD CONSTRAINT fk_t_fct_clients_fin_data_t_bank_fin_flows FOREIGN KEY (client_loaned_date)\n" +
+                "      REFERENCES bank_financial_flows (fin_date);");
 
         DatabaseDeployment.disconnect();
     }
